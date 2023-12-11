19f24116e8e32d03619009178d0acbb181028d4b
KavishkaMaduranga <kavishkamaduranga2000kmn@gmail.com>
2023-12-12 00:17:53 +0530

    Cplourp

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function Underline(){
  function italic(){
     document.execCommand('italic',true,null)
 }
+
+//color picker
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('colorPickerButton').addEventListener('click',colorPickeropen);
+});
+
+function colorPickeropen() {
+    const colorPicker = document.getElementById('fontColorPicker');
+    colorPicker.click();
+}
+
+function changeFontColor() {
+    const colorPicker = document.getElementById('fontColorPicker');
+    const selectedColor = colorPicker.value;
+    document.execCommand('foreColor', true, selectedColor);
+}
